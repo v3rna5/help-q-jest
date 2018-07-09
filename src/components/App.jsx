32ae28f6cf365510e6930b0beefcf373dbe5b087
verna <vernajs@gmail.com>
2018-07-09 15:57:42 -0700

    Test

---
+++ src/components/App.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import Header from './Header';
+import TicketList from './TicketList';
+import NewTicketControl from './NewTicketControl';
+import Error404 from './Error404';
+import { Switch, Route } from 'react-router-dom';
+import Moment from 'moment';
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      masterTicketList: []
+    };
+    this.handleAddingNewTicketToList = this.handleAddingNewTicketToList.bind(this);
+  }
+
+  componentDidMount() {
+    this.waitTimeUpdateTimer = setInterval(() =>
+      this.updateTicketElapsedWaitTime(),
+    5000
+    );
+  }
+
+  componentWillUnmount(){
+    clearInterval(this.waitTimeUpdateTimer);
+  }
+
+  updateTicketElapsedWaitTime() {
+    let newMasterTicketList = this.state.masterTicketList.slice();
+    newMasterTicketList.forEach((ticket) =>
+      ticket.formattedWaitTime = (ticket.timeOpen).fromNow(true)
+    );
+    this.setState({masterTicketList: newMasterTicketList});
+  }
+
+  handleAddingNewTicketToList(newTicket){
+    var newMasterTicketList = this.state.masterTicketList.slice();
+    newTicket.formattedWaitTime = (newTicket.timeOpen).fromNow(true);
+    newMasterTicketList.push(newTicket);
+    this.setState({masterTicketList: newMasterTicketList});
+  }
+
+  render(){
+    return (
+      <div>
+        <Header/>
+        <Switch>
+          <Route exact path='/' render={()=><TicketList ticketList={this.state.masterTicketList} />} />
+          <Route path='/newticket' render={()=><NewTicketControl onNewTicketCreation={this.handleAddingNewTicketToList} />} />
+          <Route component={Error404} />
+        </Switch>
+      </div>
+    );
+  }
+
+}
+
+export default App;
---
+++ src/components/TicketList.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Ticket from './Ticket';
+import PropTypes from 'prop-types';
+
+function TicketList(props){
+  return (
+    <div>
+      <hr/>
+      {props.ticketList.map((ticket) =>
+        <Ticket names={ticket.names}
+          location={ticket.location}
+          issue={ticket.issue}
+          formattedWaitTime={ticket.formattedWaitTime}
+          key={ticket.id}/>
+      )}
+    </div>
+  );
+}
+
+TicketList.propTypes = {
+  ticketList: PropTypes.array
+};
+
+export default TicketList;
