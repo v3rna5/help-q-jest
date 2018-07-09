a698df661cce119d4d37689c4daf7ade141a36d0
verna <vernajs@gmail.com>
2018-07-09 16:53:59 -0700

    Adding Redux to React

--- src/components/NewTicketForm.jsx
+++ src/components/NewTicketForm.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { v4 } from 'uuid';
 import Moment from 'moment';
+import { connect } from 'react-redux';
 
 function NewTicketForm(props){
   let _names = null;
@@ -9,8 +8,17 @@ function NewTicketForm(props){
   let _issue = null;
 
   function handleNewTicketFormSubmission(event) {
+    const { dispatch } = props;
     event.preventDefault();
-    props.onNewTicketCreation({names: _names.value, location: _location.value, issue: _issue.value, id: v4(), timeOpen: new Moment()});
+    const action = {
+      type: 'ADD_TICKET',
+      id: null,
+      names: _names.value,
+      location: _location.value,
+      issue: _issue.value,
+      timeOpen: new Moment()
+    };
+    dispatch(action);
     _names.value = '';
     _location.value = '';
     _issue.value = '';
@@ -39,8 +47,4 @@ function NewTicketForm(props){
   );
 }
 
-NewTicketForm.propTypes = {
-  onNewTicketCreation: PropTypes.func
-};
-
-export default NewTicketForm;
+export default connect()(NewTicketForm);
